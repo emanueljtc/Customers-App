5c99d0b72ce8865789dc995aa77ef4e8a8c8b543
ema18787 <emanueljtc@gmail.com>
2018-08-20 14:30:53 -0400

    importando customerContainer

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomeContainer from './containers/HomeContainer';
-import CustomersContainer from './containers/CustomersContainer';
+import CustomersContainers from './containers/CustomersContainer';
+import CustomerContainer from './containers/CustomerContainer';
 
 class App extends Component {
 
   renderHome = () => <HomeContainer />;
 
-  renderCustomerContainer = () => <h2>Customer Container</h2>;
+  renderCustomersContainer = () => <CustomersContainers />;
 
   renderCustomerListContainer = () => <h3>Customer List Container</h3>;
 
-  renderCustomerNewContainer = () => <h4>Customer New Container</h4>;
+  renderCustomerNewContainer = () => <CustomerContainer />;
   render() {
     return (
       <Router>
           <div>
             <Route exact path="/" component={this.renderHome} />
-          <Route exact path="/customers" component={CustomersContainer} />
+          <Route exact path="/customers" component={this.renderCustomersContainer} />
             <Switch>
-              <Route path="/customers/new" component={CustomersContainer} />
-              <Route path="/customers/:dni" component={this.renderCustomerContainer} />
+              <Route path="/customers/new" component={this.renderCustomerNewContainer} />
+              <Route path="/customers/:dni" component={CustomerContainer} />
             </Switch>
           </div>
       </Router>
