e9c37f0b8a1d51a3811f6e69e1a1af48f41bfc3c
ema18787 <emanueljtc@gmail.com>
2018-08-20 12:40:48 -0400

    vinculando json-server

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,27 +1,7 @@
 import { FETCH_CUSTOMERS } from './../constants';
 import { createAction } from 'redux-actions';
 
-const customers = [
-  {
-    "dni": 20355749,
-    "name": "Ana Cordova",
-    "age": 26
-  },
-  {
-    "dni": 18971787,
-    "name": "Emanuel Torres",
-    "age": 28
-  },
-  {
-    "dni": 5161055,
-    "name": "Joaquin Torres",
-    "age": 59
-  },
-  {
-    "dni": 8784453,
-    "name": "Ana Clemente.",
-    "age": 54
-  },
-];
+const url = 'http://localhost:3001/customers';
 
-export const fetchCustomers = createAction (FETCH_CUSTOMERS, () => customers);
+const apiFetchCustomers = () => fetch(url).then(v => v.json());
+export const fetchCustomers = createAction (FETCH_CUSTOMERS, apiFetchCustomers);
