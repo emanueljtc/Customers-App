fa98b81d3dcc1c41ddec463195ff6ae28aef921b
ema18787 <emanueljtc@gmail.com>
2018-08-21 14:13:53 -0400

    operador ternario para customerControl

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,14 +3,25 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import { getCustomerByDni } from "./../selectors/customers";
-
+import { Route } from 'react-router-dom';
+import CustomerEdit from './../components/CustomerEdit'
+import CustomersData from '../components/CustomersData';
 class CustomerContainer extends Component {
-  
+  renderBody = () => (
+    <Route path="/customers/:dni/edit" children={
+      ( { match } ) => {
+          const CustomerControl = match ? CustomerEdit : CustomersData ;
+          return <CustomerControl {...this.props.customer}/> // Spread Attributes
+        
+      }
+    } />
+  )
+  // <p>Datos del Cliente {this.props.customer.name}</p>
   render() {
     return (
       <div>
           <AppFrame header={`Cliente ${this.props.dni}`}
-            body={<p>Datos del Cliente {this.props.customer.name}</p>}>
+            body={this.renderBody()}>
           </AppFrame>
       </div>
     );
