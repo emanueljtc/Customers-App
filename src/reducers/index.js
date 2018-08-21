e556099fe243f66dd96fcddf21596ed7b275db07
ema18787 <emanueljtc@gmail.com>
2018-08-21 14:38:42 -0400

    aplicando el uso de reduxForm

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 // Dependencias
 import { combineReducers } from 'redux';
 import { customers } from './customers';
+import {reducer as reduxForm } from 'redux-form';
 
 export default combineReducers({
-    customers
+    customers,
+    form: reduxForm
 });
